8a6462bc590263d349c1fd53c2189e7c0da75f87
Davide Cesari <dcesari@arpae.it>
2016-08-30 07:45:15 +0000

    Generic conf files

---
+++ conf.sh
@@ -0,0 +1,11 @@
+# ARKIMET
+ARKI_DIR=/arkimet/arkimet
+ARKI_CONF=$ARKI_DIR/config
+ARKI_SCAN_METHOD=arki_importer
+ARKI_IMPDIR=~arki-imp/arki-imp
+#
+ARKI_USE_INOTIFY=Y
+
+# GRIB coding
+GRIB_CENTER=80
+
