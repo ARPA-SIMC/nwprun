619bd5c9d598cbc579cdafd640745530b925d3c4
Davide Cesari <dcesari@arpae.it>
2016-08-31 12:24:49 +0200

    add conf for radar_lhn at 2.2 km

--- conf.sh
+++ conf.sh
@@ -1,21 +1,25 @@
 case "$HOSTNAME" in
-  *.metarpa | maialinux )
+  *.metarpa | maialinux ) # Arpae
 # General
     WORKDIR=$OPE/$NWPCONF
     LOGDIR=$SCRATCH/log
-# ARKIMET
+# Arkimet
     ARKI_DIR=$OPE/arkimet
     ARKI_CONF=$ARKI_DIR/config
     ARKI_SCAN_METHOD=arki_importer
     ARKI_IMPDIR=$ARKI_DIR/inbound
     ARKI_URL=http://maialinux.metarpa:8090
     ARKI_USE_INOTIFY=Y
+# Radar
+    RADAR_MOSAICODIR=$HOME/prelhn/Composito
+    RADAR_LHNDIR=$HOME/prelhn/bufr2grib-RUC
+    RADAR_DT=10
     ;;
   *.novalocal ) # Cineca cloud
 # General
     WORKDIR=$HOME/$NWPCONF
     LOGDIR=$HOME/log
-# ARKIMET
+# Arkimet
     ARKI_DIR=/arkimet/arkimet
     ARKI_CONF=$ARKI_DIR/config
     ARKI_SCAN_METHOD=arki_importer
@@ -27,7 +31,7 @@ case "$HOSTNAME" in
 # General
     WORKDIR=$CINECA_SCRATCH/$NWPCONF
     LOGDIR=$CINECA_SCRATCH/log
-# ARKIMET
+# Arkimet
     ARKI_SCAN_METHOD=remote_arki_importer
 #
     ARKI_IMPDIR=~arki-imp/arki-imp
---
+++ prod/RADAR_LHN_IT2/conf.sh
@@ -0,0 +1,9 @@
+RADAR_LHN_WORKDIR=$WORKDIR/radar_lhn
+RADAR_MOSAICOCONF=$RADAR_MOSAICODIR/configurazioni/COSMO_IT2.CONF
+RADAR_LHN_GP=10
+# reconfigure arkimet for remote archiving at Cineca
+ARKI_SCAN_METHOD=remote_arki_importer
+#
+ARKI_IMPDIR=~arki-imp/arki-imp
+#
+
