c4d86a261e9642c0c981f6da7723749044fd5be7
Davide Cesari <dcesari@arpae.it>
2016-08-31 12:22:37 +0200

    fixes for radar_lhn

--- radar_lhn_get_it2.sh
+++ radar_lhn_get_it2.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 unset LANG
-basedir=$HOME/ecflow
+basedir=$HOME/ope
 # setup common to user scripts
 # basic variables
 export NWPCONFDIR=$basedir/conf
 export NWPCONFBINDIR=$basedir/libexec/nwpconf
-export NWPCONF=prod/COSMO_IT2
+export NWPCONF=prod/RADAR_LHN_IT2
 
 set -e
 # source the main library module
 . $NWPCONFBINDIR/nwpconf.sh
 # source other optional modules
-. $NWPCONFBINDIR/cosmo_model.sh
 . $NWPCONFBINDIR/putarki.sh
 . $NWPCONFBINDIR/simc_site.sh
 # end of setup
-#set -x
+
+# redirect all to logfile
+exec >>$LOGDIR/`basename $0`.log 2>&1
+
+set -x
 
 [ -n "$RADAR_LHN_WORKDIR" ] || exit 1
 mkdir -p $RADAR_LHN_WORKDIR
@@ -34,7 +37,7 @@ if [ -n "$1" ]; then
 else
     restore_state radar_lhn_get.state
     # initial date from end of previous run or yesterday at 12
-    [ -n "$lastdate" ] || lastdate=`date -u --date '1 day ago' '+%Y%m%d12'`
+    [ -n "$lastdate" ] || lastdate=`date -u --date '1 day ago' '+%Y%m%d1200'`
     todate=`date -u '+%Y%m%d%H%M'`
     lastdate=`simc_create_radar_grib $lastdate $todate`
     [ -n "$lastdate" ] && save_state radar_lhn_get.state lastdate
