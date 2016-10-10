676af42b7a235d3724d1d158608c61f2f1a9c070
Davide Cesari <dcesari@arpae.it>
2016-10-10 16:48:36 +0000

    add evaluation of ens mode

--- conf.sh
+++ conf.sh
@@ -24,7 +24,7 @@ case "$HOSTNAME" in
     ARKI_CONF=$ARKI_DIR/config
     ARKI_SCAN_METHOD=arki_importer
     ARKI_IMPDIR=~arki-imp/arki-imp
-#
+    ARKI_URL=http://localhost:8090
     ARKI_USE_INOTIFY=Y
     ;;
   node* ) # Cineca HPC
@@ -41,4 +41,13 @@ esac
 
 # GRIB coding
 GRIB_CENTER=80
+BUFR_ARKI_DS="$ARKI_URL/dataset/cnmc_acars $ARKI_URL/dataset/cnmc_airep $ARKI_URL/dataset/cnmc_amdar $ARKI_URL/dataset/cnmc_buoy $ARKI_URL/dataset/cnmc_pilot $ARKI_URL/dataset/cnmc_ship11 $ARKI_URL/dataset/cnmc_ship13 $ARKI_URL/dataset/cnmc_ship9 $ARKI_URL/dataset/cnmc_synop1 $ARKI_URL/dataset/cnmc_synop3 $ARKI_URL/dataset/cnmc_temp $ARKI_URL/dataset/cnmc_tempship $ARKI_URL/dataset/cnmc_wprof"
+
+# ensemble prediction
+ENS_MODE=.FALSE.
+if [ -n "$ENS_TOTAL_MEMB" ]; then
+    if [ "$ENS_TOTAL_MEMB" -gt 1 -a -n "$ENS_MEMB" ]; then
+        ENS_MODE=.TRUE.
+    fi
+fi
 
