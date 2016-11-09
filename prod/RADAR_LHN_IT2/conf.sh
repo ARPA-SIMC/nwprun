d48f26905e27964c2ce5049b4606db6b23bbbced
Davide Cesari <dcesari@arpae.it>
2016-11-09 14:46:42 +0000

    modification for radar on maialinux

--- prod/RADAR_LHN_IT2/conf.sh
+++ prod/RADAR_LHN_IT2/conf.sh
@@ -1,8 +1,8 @@
 RADAR_LHN_WORKDIR=$WORKDIR/radar_lhn
-RADAR_MOSAICOCONF=$RADAR_MOSAICODIR/configurazioni/COSMO_IT2.CONF
+RADAR_MOSAICOCONF=$RADAR_MOSAICODIR/configurazioni/cosmo_2I.CONF
 RADAR_LHN_GP=10
 # reconfigure arkimet for remote archiving at Cineca
 ARKI_SCAN_METHOD=remote_arki_importer
 #
-ARKI_IMPDIR=~arki-imp/arki-imp
+ARKI_IMPDIR=~arki-imp/arki-imp/generic
 
