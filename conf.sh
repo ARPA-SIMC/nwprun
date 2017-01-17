480d01f9255b8e6430198df934fe64bb61e4db6b
Davide Cesari <dcesari@arpae.it>
2017-01-17 19:09:35 +0100

    new amdar dataset and configuration of forecast

--- conf.sh
+++ conf.sh
@@ -43,7 +43,7 @@ esac
 
 # GRIB coding
 GRIB_CENTER=80
-BUFR_ARKI_DS="$ARKI_URL/cnmc_acars $ARKI_URL/cnmc_airep $ARKI_URL/cnmc_amdar $ARKI_URL/cnmc_buoy $ARKI_URL/cnmc_pilot $ARKI_URL/cnmc_ship11 $ARKI_URL/cnmc_ship13 $ARKI_URL/cnmc_ship9 $ARKI_URL/cnmc_synop1 $ARKI_URL/cnmc_synop3 $ARKI_URL/cnmc_temp $ARKI_URL/cnmc_tempship $ARKI_URL/cnmc_wprof"
+BUFR_ARKI_DS="$ARKI_URL/cnmc_acars $ARKI_URL/cnmc_airep $ARKI_URL/cnmc_amdar $ARKI_URL/cnmc_amdar146 $ARKI_URL/cnmc_buoy $ARKI_URL/cnmc_pilot $ARKI_URL/cnmc_ship11 $ARKI_URL/cnmc_ship13 $ARKI_URL/cnmc_ship9 $ARKI_URL/cnmc_synop1 $ARKI_URL/cnmc_synop3 $ARKI_URL/cnmc_temp $ARKI_URL/cnmc_tempship $ARKI_URL/cnmc_wprof"
 
 # ensemble prediction
 ENS_MODE=.FALSE.
