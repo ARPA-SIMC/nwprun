94aab06581807b0090eca08730ca28b1e5c26632
Davide Cesari <dcesari@arpae.it>
2018-02-19 14:11:19 +0100

    new suite cosmo_28N_assim (assimilation for ruc)

--- ecflow/arpae_suites.py
+++ ecflow/arpae_suites.py
@@ -80,6 +80,44 @@ cosmo_5i_fcast.check()
 cosmo_5i_fcast.write()
 cosmo_5i_fcast.replace()
 
+# suite cosmo_28n_assim
+basicenv = BasicEnv(srctree=os.environ["OPE"],
+                    worktree=os.path.join(os.environ["WORK"], "ecflow"),
+                    sched="slurm",
+                    client_wrap="",
+                    ntries=1,
+                    extra_env={
+                        "NWPCONF": "prod/cosmo_28N/assim",
+                        "NNODES_MODEL": 4,
+                        "EXTRA_SCHED": "",
+                        "NO_FAIL": "FALSE",
+                        "TASK_PER_CORE": "1"
+                    })
+
+cosmo_28n_assim = ModelSuite("cosmo_28N_assim")
+basicenv.add_to(cosmo_28n_assim.suite)
+day = cosmo_28n_assim.suite.add_family("day").add_repeat(
+    ecflow.RepeatDate("YMD", 
+                      int(datetime.datetime.now().strftime("%Y%m%d")),
+                      20201228))
+
+hdep = None # first repetition has no dependency
+for h in range(0, 24, 3):
+    famname = "hour_" + ("%02d" % h)
+    hour = day.add_family(famname).add_variable("TIME", "%02d" % h)
+    #    hrun = "%02d:00" % (h+1 % 24) # start 1h after nominal time
+    WaitAndRun(dep=hdep, runlist=[
+        GetObs(gts=True, lhn=True),
+        EpsMembers(membrange="0", postprocrange="0", wait_obs=True),
+        ContinuousAnalysis()
+    ]
+    ).add_to(hour)
+    hdep = famname # dependency for next repetition
+
+cosmo_28n_assim.check()
+cosmo_28n_assim.write()
+cosmo_28n_assim.replace()
+
 # suite 28n_reassim_era
 basicenv = BasicEnv(srctree=os.environ["OPE"],
                     worktree=os.path.join(os.environ["WORK"], "ecflow"),
