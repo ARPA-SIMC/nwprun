d350b538222f5540b48ee39e65f752aa13a5563d
Davide Cesari <dcesari@arpae.it>
2019-06-25 10:03:17 +0200

    eliminate last occurences of $WORK

--- ecflow/include_slurm/galileo/sched_serial_load.h
+++ ecflow/include_slurm/galileo/sched_serial_load.h
@@ -6,5 +6,5 @@ module load intel
 module load hdf5
 module load intelmpi
 module load mkl
-export LD_LIBRARY_PATH=$WORK/srcintel/install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$WORKDIR_BASE/srcintel/install/lib:$LD_LIBRARY_PATH
 
