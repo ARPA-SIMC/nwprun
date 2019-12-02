d5a0f7b8f7f432659813ab3182579defaeb02d20
Davide Cesari <dcesari@arpae.it>
2019-12-02 10:52:40 +0100

    add cosmo_2I test suites

---
+++ conf/test/cosmo_2I/conf.sh
@@ -0,0 +1,73 @@
+# RADAR for LHN
+ARKI_LHN_DS=$ARKI_URL/cosmo_2I_radar
+#RADAR_LHN_GP=10
+MODEL_LHN=.FALSE.
+MODEL_LHN_WORKDIR=$WORKDIR/lhn
+MODEL_LHN_DT=10
+
+# observations for nudging
+BUFR_WORKDIR=$WORKDIR/bufr
+MODEL_NUDG=.TRUE.
+
+# _GP=10(radarobs),11(an),12(fc)
+# definitions for parent model
+PARENTMODEL=IFS
+PARENTMODEL_DATADIR=$WORKDIR/input/data
+PARENTMODEL_ARKI_DS=$ARKI_URL/ifs_med
+PARENTMODEL_FREQINI=6
+PARENTMODEL_FREQANA=6
+PARENTMODEL_FREQFC=3
+PARENTMODEL_QRQS=.FALSE.
+
+# definitions for the model run
+MODEL_NLEV=65
+# preprocessing (interpolation)
+MODEL_PRE_WORKDIR=$WORKDIR/int2lm
+MODEL_PRE_DATADIR=$WORKDIR/int2lm/data
+MODEL_PRE_BIN=$WORKDIR_BASE/srcintel/int2lm_170406_2.04a/tstint2lm
+# model run
+MODEL_WORKDIR=$WORKDIR/cosmo
+MODEL_DATADIR=$WORKDIR/cosmo/data
+MODEL_BIN=$WORKDIR_BASE/srcintel/cosmo_180802_5.05_1/lmparbin_all
+MODEL_STATIC=$WORKDIR_BASE/data/cosmo
+MODEL_PUBLISH_CONF=Y
+
+# configuration of assimilation
+#MODEL_BCANA=Y
+MODEL_SOIL_PARENT=N
+MODEL_SNOW_PARENT=N
+MODEL_LAKE=Y
+MODEL_SLOW_PAST_H=144
+MODEL_FREQINI=3
+# difference (hours) between $DATE$TIME (end of assimilation window /
+# start of forecast) and start of last available input forecast
+# providing BC (for MODEL_BCANA=N)
+# in reassim this is overridden in conf.sh because of MODEL_BCANA
+# 00->12 12->00 => 12
+# 06->00 18->12 => 6
+if [ "$TIME" = "00" -o "$TIME" = "12" ]; then
+    MODEL_DELTABD=12
+elif [ "$TIME" = "09" -o "$TIME" = "21" ]; then
+    MODEL_DELTABD=9
+elif [ "$TIME" = "06" -o "$TIME" = "18" ]; then
+    MODEL_DELTABD=6
+else # 03 15
+    MODEL_DELTABD=3
+fi
+
+# for distinguishing assimilation from forecast results
+MODEL_ARKI_TIMERANGE_ASSIM="timerange:Timedef,0,254;proddef:GRIB:tod=0"
+MODEL_ARKI_TIMERANGE_FCAST="timerange:Timedef,0,254;proddef:GRIB:tod=1"
+ARKI_DS_ASSIM=$ARKI_URL/cosmo_2I_assim
+ARKI_DS_FCAST=$ARKI_URL/cosmo_2I_fcast
+ARKI_DS_INTER=$ARKI_URL/cosmo_2I_inter
+MODEL_ASSIM_GP=11
+MODEL_FCAST_GP=12
+MODEL_FCRUC_GP=14
+MODEL_FCENS_GP=15
+MODEL_INTER_GP=13
+ANA_EXT=0000
+ANA_DET_EXT=.det
+
+ARKI_SCAN_METHOD=arki-scan
+ARKI_CONF=$WORKDIR/arkimet
---
+++ ecflow/cineca_test_suites.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+import os,sys
+import datetime
+import optparse
+import ecflow
+from nwprun import *
+
+parser = optparse.OptionParser(usage="%prog [OPTIONS]")
+parser.add_option("--yes", help="work in non-interactive mode and answer yes to all questions (it will overwrite files and replace scripts on server)",
+                  action="store_true")
+parser.add_option("--delta", help="comma-separated list of delta time in days to go back, for each suite",
+                  default="0,0,0")
+
+opts, args = parser.parse_args()
+interactive = not opts.yes
+delta = [int(i) for i in opts.delta.split(',')]
+
+common_extra_env = {
+    "NO_FAIL": "FALSE",
+    "TASK_PER_CORE": "1",
+    "HPCENV": os.environ["HPC_SYSTEM"],
+    "ECF_TIMEOUT": "7200",
+    "ECF_DENIED": ""
+}
+
+# Suite enda
+extra_env = common_extra_env.copy()
+extra_env.update({
+    "NWPCONF": "test/cosmo_2I/enda",
+    "NNODES_MODEL": 2,
+    "NNODES_ENDA": 4
+})
+basicenv = BasicEnv(srctree=os.environ["OPE"],
+                    worktree=os.path.join(os.environ["WORKDIR_BASE"], "ecflow"),
+                    sched="slurm",
+                    client_wrap=os.path.join(os.environ["OPE"],"ecflow","ec_wrap"),
+                    ntries=2,
+                    extra_env=extra_env)
+
+conf = ModelConfig({"gts": True, "lhn": True, "membrange": "0-40",
+                    "postprocrange": "0",
+                    "runlist": [GetObs, EpsMembers, EndaAnalysis]}).getconf()
+enda = ModelSuite("cosmo_2I_enda")
+basicenv.add_to(enda.suite)
+day = enda.suite.add_family("day").add_repeat(
+    ecflow.RepeatDate("YMD", 
+                      int((datetime.datetime.now()-datetime.timedelta(days=delta[0])).strftime("%Y%m%d")),
+                      20201228))
+
+hdep = None # first repetition has no dependency
+for h in range(0, 24, 3):
+    famname = "hour_" + ("%02d" % h)
+    hour = day.add_family(famname).add_variable("TIME", "%02d" % h)
+    #    hrun = "%02d:00" % (h+1 % 24) # start 1h after nominal time
+    WaitAndRun(dep=hdep, conf=conf).add_to(hour)
+    hdep = famname # dependency for next repetition
+
+enda.check()
+enda.write(interactive=interactive)
+enda.replace(interactive=interactive)
+
+# Suite fcruc
+extra_env = common_extra_env.copy()
+extra_env.update({
+    "NWPCONF": "test/cosmo_2I/fcruc",
+    "NNODES_MODEL": 12,
+    "NNODES_ENDA": 4
+})
+basicenv = BasicEnv(srctree=os.environ["OPE"],
+                    worktree=os.path.join(os.environ["WORKDIR_BASE"], "ecflow"),
+                    sched="slurm",
+                    client_wrap=os.path.join(os.environ["OPE"],"ecflow","ec_wrap"),
+                    ntries=2,
+                    extra_env=extra_env)
+
+conf = ModelConfig({"gts": True, "lhn": True, "membrange": "0",
+                    "postprocrange": "0",
+                    "runlist": [GetObs, EpsMembers]}).getconf()
+fcruc = ModelSuite("cosmo_2I_fcruc")
+basicenv.add_to(fcruc.suite)
+day = fcruc.suite.add_family("day").add_repeat(
+    ecflow.RepeatDate("YMD", 
+                      int((datetime.datetime.now()-datetime.timedelta(days=delta[1])).strftime("%Y%m%d")),
+                      20201228))
+
+hdep = None # first repetition has no dependency
+for h in range(0, 24, 3):
+    famname = "hour_" + ("%02d" % h)
+    hour = day.add_family(famname).add_variable("TIME", "%02d" % h)
+    #    hrun = "%02d:00" % (h+1 % 24) # start 1h after nominal time
+    WaitAndRun(dep=hdep, conf=conf).add_to(hour)
+    hdep = famname # dependency for next repetition4
+
+fcruc.check()
+fcruc.write(interactive=interactive)
+fcruc.replace(interactive=interactive)
+
+# Suite fcens
+extra_env = common_extra_env.copy()
+extra_env.update({
+    "NWPCONF": "test/cosmo_2I/fcens",
+    "NNODES_MODEL": 6,
+    "NNODES_ENDA": 4,
+    "ECF_TIMEOUT": "14400"
+})
+basicenv = BasicEnv(srctree=os.environ["OPE"],
+                    worktree=os.path.join(os.environ["WORKDIR_BASE"], "ecflow"),
+                    sched="slurm",
+                    client_wrap=os.path.join(os.environ["OPE"],"ecflow","ec_wrap"),
+                    ntries=2,
+                    extra_env=extra_env)
+
+conf = ModelConfig({"gts": True, "lhn": True, "membrange": "1-20",
+                    "postprocrange": "1-20",
+                    "runlist": [GetObs, EpsMembers, EpsPostproc]}).getconf()
+fcens = ModelSuite("cosmo_2I_fcens")
+basicenv.add_to(fcens.suite)
+day = fcens.suite.add_family("day").add_repeat(
+    ecflow.RepeatDate("YMD", 
+                      int((datetime.datetime.now()-datetime.timedelta(days=delta[2])).strftime("%Y%m%d")),
+                      20201228))
+
+hdep = None # first repetition has no dependency
+for h in range(21, 24, 3): # h=21
+    famname = "hour_" + ("%02d" % h)
+    hour = day.add_family(famname).add_variable("TIME", "%02d" % h)
+    #    hrun = "%02d:00" % (h+1 % 24) # start 1h after nominal time
+    WaitAndRun(dep=hdep, conf=conf).add_to(hour)
+    hdep = famname # dependency for next repetition
+
+fcens.check()
+fcens.write(interactive=interactive)
+fcens.replace(interactive=interactive)
+
