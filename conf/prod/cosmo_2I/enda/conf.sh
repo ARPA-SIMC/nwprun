0ca9e096e3a6a3d9d0c6abb16cff536a685cd5fa
Davide Cesari <dcesari@arpae.it>
2020-04-14 11:57:34 +0200

    revert to parallel read also on meucci (change made on 20200206)

--- conf/prod/cosmo_2I/enda/conf.sh
+++ conf/prod/cosmo_2I/enda/conf.sh
@@ -1,4 +1,5 @@
 MODEL_BIN=$WORKDIR_BASE/srcintel/cosmo_180802_5.05_1_dp/lmparbin_all
+#MODEL_BIN=$WORKDIR_BASE/srcintel/cosmo_180802_5.05_1_debug/lmparbin_all
 PARENTMODEL=COSMO
 MODEL_NUDG=.FALSE.
 MODEL_LHN=.TRUE.
