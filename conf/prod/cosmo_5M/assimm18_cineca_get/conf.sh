24abe0f8242994bb53b14147bd2c6b029d92ff11
Davide Cesari <dcesari@arpae.it>
2020-07-09 14:16:24 +0200

    add configuration for processing model output on Cineca clusters

---
+++ conf/prod/cosmo_5M/assimm18_cineca_get/conf.sh
@@ -0,0 +1,33 @@
+MODEL_SIGNAL=cosmo_5M_assimm18
+ARKI_IMPDIR=$WORKDIR_BASE/download
+# modificare
+LAMI_CINECA_WORKDIR=$WORKDIR_BASE/download/work/$MODEL_SIGNAL
+
+# $TIME deve essere protetto perche' non e' ancora noto, lo sviluppo con eval
+if [ "$HPC_SYSTEM" = "galileo" ]; then
+  CINECA_SUITEDIR='/gpfs/meteo/LM_WorkDir/galileo/lm/PROD/work/$TIME/lm5/ope/analysis_m18h'
+  CINECA_GRIBOUTDIR='$CINECA_SUITEDIR/dataoutput'
+else
+  CINECA_SUITEDIR='/meucci_meteo/LM_WorkDir/galileo/lm/PROD/work/$TIME/lm5/ope/analysis_m18h'
+  CINECA_GRIBOUTDIR='$CINECA_SUITEDIR/dataoutput'
+fi
+
+# quanti secondi aspetto un determinato file prima di passare al
+# successivo
+#NWPWAITELAPS=1800
+# quanti secondi aspetto dopo il tempo nominale per cominciare a
+# cercare un certo dato
+NWPWAITSOLAR_RUN=7200
+# quanti secondi aspetto dopo il tempo nominale per rinunciare a
+# cercare un certo dato e passare al successivo
+# questo dovrebbe essere consistente con NWPWAITSOLAR e MODEL_DELTABD
+# impostati in cosmo_2I/enda/conf.sh
+NWPWAITSOLAR=57600
+# durata di ogni ciclo di attesa
+NWPWAITWAIT=60
+
+# caratteristiche della suite
+MODEL_FREQINI=12
+MODEL_BACK=18
+MODEL_DELTABD=0
+MODEL_STOP=6
