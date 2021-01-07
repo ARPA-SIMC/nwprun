08766ffe2dfdbb6fecb3538d1ffae2b5ba654ac6
Thomas Gastaldo <tgastald@r033c01s03.galileo.cineca.it>
2021-01-07 14:54:19 +0100

    Modified COSMO bin path for "enda_radvol" suite to include EMVORADO

--- conf/prod/cosmo_2I/enda/conf.sh
+++ conf/prod/cosmo_2I/enda/conf.sh
@@ -1,5 +1,4 @@
-MODEL_BIN=$WORKDIR_BASE/srcintel/cosmo_180802_5.05_1_dp/lmparbin_all
-#MODEL_BIN=$WORKDIR_BASE/srcintel/cosmo_180802_5.05_1_debug/lmparbin_all
+MODEL_BIN=$WORKDIR_BASE/srcintel/cosmo_180802_5.05_1_dp_emvorado/radarbin_nud
 PARENTMODEL=COSMO
 MODEL_NUDG=.FALSE.
 MODEL_LHN=.TRUE.
