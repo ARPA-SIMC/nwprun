2a02802b41cc3560a5b976e88a1c319f603c731f
Davide Cesari <dcesari@arpae.it>
2021-08-09 11:34:03 +0200

    introduce common get_ code and convert gts_bufr_get procedure

---
+++ cron/get_common.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+log() {
+    echo `date -u --rfc-3339=seconds` "|$$|$@"
+}
+
+
+increment_datetime() {
+    if [ -z "$DATE" -o -z "$TIME" ]; then # set minimum datetime
+        DATE=`date -u --date '1 day ago' '+%Y%m%d'`
+        TIME=1200
+    else
+        DATE=`date --date="$DATE $TIME $PROC_STEP seconds" '+%Y%m%d'`
+        TIME=`date --date="$DATE $TIME $PROC_STEP seconds" '+%H%M'`
+    fi
+}
+
+
+trap_setup() {
+    # setup kill, exit and reload traps
+    mustexit=
+    mustreload=
+    trap '{ mustexit=Y; }' 15 20 2
+    trap '{ mustreload=Y; }' 1
+    trap '{ final_cleanup; }' EXIT
+}
+
+
+trap_check() {
+    # check if traps have to be called
+    [ -n "$mustexit" ] && exit 1 || true
+    [ -n "$mustreload" ] && exec "$0" "$@" || true
+}
+
+
+final_cleanup() {
+    trap - EXIT
+    exit
+}
+
+
+main_loop() {
+    get_init
+
+    unset LANG
+    basedir=$WORKDIR_BASE/nwprun
+    # setup common to user scripts
+    # basic variables
+    export NWPCONFDIR=$basedir/conf
+    export NWPCONFBINDIR=$basedir/libexec/nwpconf
+    export NWPCONF=prod/$PROCNAME
+
+    set -x
+    set -e
+    # source the main library module
+    . $NWPCONFBINDIR/nwpconf.sh
+    # source other optional modules
+    . $NWPCONFBINDIR/putarki.sh
+    . $NWPCONFBINDIR/arki_tools.sh
+    . $NWPCONFBINDIR/nwpwait.sh
+    # end of setup
+
+    PROC_WORKDIR=$WORKDIR
+    # improve
+    if [ -n "$1" ]; then # interactive run
+	PROC_WORKDIR=${PROC_WORKDIR}_interactive
+	DATETIME=$1
+	DATE=${DATETIME:0:8}
+	TIME=${DATETIME:8:4}
+    else # automatic run
+	nonunique_exit
+	# redirect all to logfile
+	exec >>$LOGDIR/`basename $0`.log 2>&1
+
+# setup kill traps
+	trap_setup
+
+    fi
+
+    safe_rm_rf $PROC_WORKDIR
+    mkdir -p $PROC_WORKDIR
+    cd $PROC_WORKDIR
+
+    while true; do
+	restore_state $PROCNAME.state || touch $NWPCONFDIR/$NWPCONF/$PROCNAME.state
+	increment_datetime
+
+	# exit or wait if too early, depending on daemon mode
+	if [ "$DAEMON" = Y ]; then
+	    wait_run
+	    trap_check
+	else
+	    check_run || exit 0
+	fi
+	
+	nwpwait_setup
+	get_setup
+	while true; do
+	    res=0
+            get_one || res=$?
+	    if [ "$res" = 0 ]; then # improve $res management
+		get_cleanup # correct here?
+		break
+	    fi
+            if nwpwait_wait; then
+		trap_check
+	    else
+		break
+            fi
+	done
+	#    get_cleanup
+	save_state $PROCNAME.state DATE TIME
+    done
+}
--- cron/gts_bufr_get.sh
+++ cron/gts_bufr_get.sh
@@ -1,95 +1,31 @@
 #!/bin/bash
-# script for downloading the new bufr provided by COMET (CNMCA) since 2021
 
-log() {
-    echo `date -u --rfc-3339=seconds` "|$$|$@"
+# source common get_ procedures
+. get_common.sh
+
+# define custom functions
+get_init() {
+    export PROCNAME=gts_bufr_get
 }
 
-dl_ftp() {
+get_setup() {
+    echo putarki_configured_setup $PROCNAME "reftime=$DATE$TIME" "format=bufr" "signal=gts_bufr"
+}
 
-    file="${FTPDIR}BUFR0001${DATETIME}.DAT"
+get_cleanup() {
+    echo putarki_configured_end $PROCNAME
+}
+
+get_one() {
+    file="${FTPDIR}BUFR0001${DATE}${TIME:0:2}.DAT"
     rm -f $file
-    ncftpget -V $ncftpauth . $file || true
+    ncftpget -V -f $WORKDIR_BASE/nwprun/.auth/bufr_ruc_get.cfg . $file || true
     if [ -f "$file" ]; then
-	putarki_configured_archive $1 $file
+	echo putarki_configured_archive $PROCNAME $file
 	return 0
     fi
     return 1
-
 }
 
-unset LANG
-basedir=$OPE
-# setup common to user scripts
-# basic variables
-export NWPCONFDIR=$basedir/conf
-export NWPCONFBINDIR=$basedir/libexec/nwpconf
-export NWPCONF=prod/GTS_BUFR
-
-set -x
-set -e
-# source the main library module
-. $NWPCONFBINDIR/nwpconf.sh
-# source other optional modules
-. $NWPCONFBINDIR/putarki.sh
-. $NWPCONFBINDIR/arki_tools.sh
-. $NWPCONFBINDIR/nwpwait.sh
-# end of setup
-
-
-# improve
-ncftpauth="-f $basedir/.auth/bufr_ruc_get.cfg"
-
-if [ -n "$1" ]; then # interactive run
-    GTS_BUFR_WORKDIR=${GTS_BUFR_WORKDIR}_interactive
-    DATETIME=$1
-    DATE=${DATETIME:0:8}
-    TIME=${DATETIME:8:2}
-else # automatic run
-    nonunique_exit
-    # redirect all to logfile
-    exec >>$LOGDIR/`basename $0`.log 2>&1
-
-    restore_state gts_bufr_get.state || touch $NWPCONFDIR/$NWPCONF/gts_bufr_get.state
-    if [ -z "$DATETIME" ]; then # set minimum datetime
-	DATETIME=`date -u --date '1 day ago' '+%Y%m%d12'`
-    else # increment datetime
-	DATETIME=`datetime_add $DATETIME $GTS_BUFR_STEP`
-    fi
-    DATE=${DATETIME:0:8}
-    TIME=${DATETIME:8:2}
-
-# wait before querying the server
-    NWPWAITSOLAR_SAVE=$NWPWAITSOLAR
-    NWPWAITSOLAR=$NWPWAITSOLAR_RUN
-    NWPWAITWAIT_SAVE=$NWPWAITWAIT
-    unset NWPWAITWAIT
-    nwpwait_setup
-    nwpwait_wait && exit 0 # too early, try next time
-
-# wait until reasonable
-    NWPWAITSOLAR=$NWPWAITSOLAR_SAVE
-    NWPWAITWAIT=$NWPWAITWAIT_SAVE
-fi
-
-safe_rm_rf $GTS_BUFR_WORKDIR
-mkdir -p $GTS_BUFR_WORKDIR
-cd $GTS_BUFR_WORKDIR
-
-dirname=gts_bufr
-putarki_configured_setup $dirname "reftime=$DATETIME" "format=bufr" "signal=gts_bufr"
-# "configext=test"
-nwpwait_setup
-
-while true; do
-    dl_ftp $dirname && break
-    nwpwait_wait || break
-done
-
-putarki_configured_end $dirname
-
-if [ -n "$1" ]; then # interactive run
-    :
-else # automatic run
-    save_state gts_bufr_get.state DATETIME
-fi
+# enter main loop
+main_loop
