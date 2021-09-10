76ff5b9bb0aaea88b2673ec856637774d15d3fec
Davide Cesari <dcesari@arpae.it>
2021-09-10 17:22:43 +0200

    changes related to g100 environment

--- ecflow/include_slurm/g100/modules.h
+++ ecflow/include_slurm/g100/modules.h
@@ -1,6 +1,7 @@
 module load autoload
 module load intel
-module load hdf5
+module load hdf5/1.10.7--intelmpi--oneapi-2021--binary
 module load intelmpi
 module load mkl
 export LD_LIBRARY_PATH=$WORKDIR_BASE/srcintel/install/lib:$LD_LIBRARY_PATH
+unset I_MPI_PMI_LIBRARY
--- ecflow/include_slurm/g100/smnd.h
+++ ecflow/include_slurm/g100/smnd.h
@@ -1,3 +1,3 @@
 . ~/smnd_profile
 #module use /g100_work/PROJECTS/spack/v0.16/modules/0.16.2/linux-centos8-skylake_avx512
-module load singularity
+#module load singularity
