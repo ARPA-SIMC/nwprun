6b3f8ac8227c825611decd55650ed95b312cd6f2
Davide Cesari <dcesari@arpae.it>
2021-09-17 12:46:01 +0200

    use self-compiled hdf5

--- ecflow/include_slurm/g100/modules.h
+++ ecflow/include_slurm/g100/modules.h
@@ -1,6 +1,6 @@
 module load autoload
 module load intel
-module load hdf5/1.10.7--intelmpi--oneapi-2021--binary
+#module load hdf5/1.10.7--intelmpi--oneapi-2021--binary
 module load intelmpi
 module load mkl
 export LD_LIBRARY_PATH=$WORKDIR_BASE/srcintel/install/lib:$LD_LIBRARY_PATH
