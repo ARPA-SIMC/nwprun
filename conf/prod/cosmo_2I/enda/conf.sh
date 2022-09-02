9ad639cb040fa1de33cfc8ef21bef145425a15ea
Davide Cesari <dcesari@arpae.it>
2022-09-02 15:21:46 +0200

    update cosmo executable for radial wind clutter removal

--- conf/prod/cosmo_2I/enda/conf.sh
+++ conf/prod/cosmo_2I/enda/conf.sh
@@ -1,4 +1,4 @@
-MODEL_BIN=/ind2/meteo/a07smr03/lami/srcintel/cosmo_5.08_radial_wind/radarbin_nud
+MODEL_BIN=/ind2/meteo/a07smr03/lami/srcintel/cosmo_5.08_radial_wind_noclut/radarbin_nud
 PARENTMODEL=COSMO
 MODEL_NUDG=.FALSE.
 MODEL_LHN=.TRUE.
