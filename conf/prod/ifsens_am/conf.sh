dd712f326eb58af8c1a21583f0dc7ab68512c6b9
Davide Cesari <dcesari@arpae.it>
2023-05-15 18:03:44 +0200

    add procedures for downloading IFS ensemble data for Kenda with Icon

---
+++ conf/prod/ifsens_am/conf.sh
@@ -0,0 +1,20 @@
+#FIRST_BC_HH=9
+#LAST_BC_HH=60
+#FREQ_BC_HH=3
+#TOT_ENS_MEMB=20
+FTPDIR=/ind2/meteoarch/a07smr01/arch_lm/g100/auto/download/OPE/AM/LAMI/IFS
+
+
+# quanti secondi aspetto un determinato file prima di passare al
+# successivo
+#NWPWAITELAPS=1800
+# quanti secondi aspetto dopo il tempo nominale per cominciare a
+# cercare un certo dato
+NWPWAITSOLAR_RUN=25200
+# quanti secondi aspetto dopo il tempo nominale per rinunciare a
+# cercare un certo dato e passare al successivo
+NWPWAITSOLAR=50400
+# durata di ogni ciclo di attesa
+NWPWAITWAIT=60
+# intervallo di disponibilita` dei dati in s
+PROC_STEP=21600
---
+++ cron/ifsens_am_enda_get.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+. `dirname $0`/get_common.sh
+
+# define custom functions
+get_init() {
+    export PROCNAME=enda_get
+    export EXTRA_CONF=ifsens_am/
+}
+
+
+get_setup() {
+    putarki_configured_setup $PROCNAME "reftime=$DATE$TIME" "format=grib" "signal=ifsens_am_enda"
+#    file_pattern="U3X????????????????1"
+# Create array of files to be downloaded
+    reftime=`datetime_cnmc $DATE$TIME`
+    for hh in 0 `seq 6 1 12`; do
+        vertime=`datetime_add $DATE$TIME $hh`
+        vertime=`datetime_cnmc $vertime`
+	file_list[$hh]="$FTPDIR/U3X${reftime}${vertime}1"
+    done
+}
+
+get_cleanup() {
+    putarki_configured_end $PROCNAME
+}
+
+get_one() {
+    while true; do
+        donenothing=Y
+
+	for i in ${!file_list[@]}; do # ! extract keys from array
+	    file=${file_list[$i]}
+            if [ -f "$file" ]; then
+                # process $file
+                log "file $file successfully downloaded and unpacked"
+                putarki_configured_archive $PROCNAME ${file}
+                log "file ${file} successfully sent to archive"
+		unset file_list[$i]
+                donenothing=
+            fi
+	done
+
+	if [ ${#file_list[@]} -eq 0 ]; then
+	    return 0
+	fi
+        if [ -n "$donenothing" ]; then
+            return 1
+        fi
+    done
+}
+
+# enter main loop
+set -x
+declare -A file_list # this must be here otherwise declare implies local
+main_loop "$@"
