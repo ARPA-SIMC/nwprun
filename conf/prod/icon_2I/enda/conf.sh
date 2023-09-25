12c1835e4621349ad39c8154d9ff1eca44694a0e
Davide Cesari <dcesari@arpae.it>
2023-09-25 15:19:21 +0200

    Add configuration files for ICON-enda

---
+++ conf/prod/icon_2I/enda/conf.sh
@@ -0,0 +1,110 @@
+# Forecast length 
+MODEL_BACK=1
+MODEL_STOP=1
+MODEL_BCANA=N
+
+# Number of ensemble members
+ENS_TOTAL_MEMB=29
+
+# Radar assimilation
+ACT_EMVORADO=.FALSE.
+RADLIST="16101 16102 16103 16105 16106 16107 16112 16144 16199 16998 16999"
+MODEL_LHN=.TRUE.
+MODEL_NH_LHN=3
+ARKI_LHN_DS=$ARKI_DIR/radar_sri
+
+# Time difference between model and parent reftime 
+case $TIME in
+    01 | 07 | 13 | 19)
+        MODEL_DELTABD=7 
+        ;;
+    02 | 08 | 14 | 20)
+        MODEL_DELTABD=8 
+        ;;
+    03 | 09 | 15 | 21)
+        MODEL_DELTABD=9 
+        ;;
+    04 | 10 | 16 | 22)
+        MODEL_DELTABD=10 
+        ;;
+    05 | 11 | 17 | 23)
+        MODEL_DELTABD=11
+        ;;
+    00 | 06 | 12 | 18)
+        MODEL_DELTABD=12
+        ;;
+esac
+
+# Environment variable definition for model and parent
+PARENTMODEL=IFS
+if [ -n "$ENS_MEMB" ]; then
+    PARENTMODEL_ARKI_DS=$ARKI_DIR/ifsens_am_foricon
+    PARENTMODEL_ARKI_DS_SST=$ARKI_DIR/hres_am_foricon
+    PARENTMODEL_SIGNAL=ifsens_am_enda
+    PARENTMODEL_SIGNAL_SST=hres_am_foricon
+    PARENTMODEL_FREQINI=6
+    PARENTMODEL_FREQANA=6
+    PARENTMODEL_FREQFC=1
+
+    # input data
+    PARENTMODEL_DATADIR=$WORKDIR/input.$ENS_MEMB/data
+    MODEL_ARKI_PARAM="proddef:GRIB:pf=$ENS_MEMB or GRIB:nn=$ENS_MEMB;origin:GRIB2,98 or GRIB1,98"
+
+    # preprocessing (interpolation)
+    MODEL_PRE_WORKDIR=$WORKDIR/preicon.$ENS_MEMB
+    MODEL_PRE_DATADIR=$WORKDIR/preicon.$ENS_MEMB/data
+
+    # model run
+    MODEL_WORKDIR=$WORKDIR/icon.$ENS_MEMB
+    MODEL_DATADIR=$WORKDIR/icon.$ENS_MEMB/data
+
+    # setup for arkilocal
+    ARKI_DIR=$WORKDIR/arki.$ENS_MEMB
+
+else # deterministic run or analysis
+    PARENTMODEL_ARKI_DS=$ARKI_DIR/hres_am_foricon
+    PARENTMODEL_SIGNAL=hres_am_foricon
+    PARENTMODEL_FREQINI=6
+    PARENTMODEL_FREQANA=6
+    PARENTMODEL_FREQFC=1
+
+    # setup for arkilocal
+    ARKI_DIR=$WORKDIR/arki
+
+    # setup for remote import and download
+    ARKI_SCAN_METHOD=configured_importer
+    unset ARKI_IMPDIR
+    ARKI_SYNCDIR=$WORKDIR_BASE/import/sync_lami
+    ARKI_DLDIR=$WORKDIR_BASE/download
+    #POSTPROC_LIST=(lami_make_nit)
+    MODEL_SIGNAL=icon_2I_assim
+fi
+MODEL_ARCHIVE_OUTPUT_ANA=$WORKDIR/archive
+
+# Environment variable definition for MEC and LETKF
+MEC_WORKDIR=$WORKDIR/mec
+LETKF_WORKDIR=$WORKDIR/letkf
+LETKF_DATADIR=$WORKDIR/letkf/data
+
+# MEC and LETKF executables
+#DACE_BASE=/ind2/meteo/a07smr03/lami/srcintel/dace_code_2.06
+DACE_BASE=/g100_work/smr_prod/srcintel/dace_code_2.15
+MEC_BIN=$DACE_BASE/build/LINUX64.intel-mpi/bin/mec
+LETKF_BIN=$DACE_BASE/build/LINUX64.intel-mpi/bin/var3d
+LETKF_CONST=$DACE_BASE/data
+
+#MODEL_SOIL_PARENT=Y
+#MODEL_SNOW_PARENT=Y
+#MODEL_LAKE=Y
+# differenza tra reftime del modello e del padre
+MODEL_SLOW_PAST_H=144
+
+NBC_PER_TASK=1
+
+# suite timing
+NWPWAITELAPS=10800
+# differenza tra tempo nominale e tempo di attivazione della suite
+NWPWAITSOLAR_RUN=3000
+# dopo quando tempo rinuncio a girare la suite e passare alla successiva
+NWPWAITSOLAR=43200
+NWPWAITWAIT=30
