189fd00c0a7e0f4c66dfd3a6e06ff1f7d200e3fb
Davide Cesari <dcesari@arpae.it>
2024-05-02 17:26:21 +0200

    implement postprocessing and import of icon_2I_fcast run by Cineca

---
+++ cron/cineca_icon_get_common.sh
@@ -0,0 +1,42 @@
+. `dirname $0`/get_common.sh
+
+
+get_post() {
+    . $NWPCONFBINDIR/lami_postproc.sh
+    PROC_STEP=$(($MODEL_FREQINI*3600))
+}
+
+get_setup() {
+    # $TIME is not known until now
+    CINECA_SUITEDIR=`eval echo $CINECA_SUITEDIR_TMPL`
+    mkdir -p $LAMI_CINECA_WORKDIR
+    # g=global required to make statuslist visible to other functions
+    declare -Ag statuslist
+    statuslist=()
+    foundrun=
+    #putarki_configured_setup $MODEL_SIGNAL "reftime=$DATE$TIME" "signal=$MODEL_SIGNAL"
+}
+
+get_cleanup() {
+    putarki_configured_end $MODEL_SIGNAL
+    safe_rm_rf $LAMI_CINECA_WORKDIR
+}
+
+get_one() {
+    #    curdate=`grep 'ydate_ini *=' $CINECA_SUITEDIR/INPUT_ORG|sed -e "s/^.*'\([0-9]*\)'.*$/\1/g"`
+    if [ -z "$foundrun" ]; then
+        if [ -f "$CINECA_SUITEDIR/icon_${DATE}${TIME}_+00000000.rf" ]; then 
+            foundrun=Y
+        else
+            return 1 # wait
+        fi
+    fi
+    
+# this seems to be the only place where it makes sense to do so ($DATE and $TIME change)
+    . $NWPCONFBINDIR/nwptime.sh
+    . $NWPCONFBINDIR/icon_model.sh
+# this is done here in case the directory is removed and recreated
+    cd $CINECA_SUITEDIR # /dataoutput
+    putarki_configured_model_output $(($MODEL_STOP + 1))
+
+}
---
+++ cron/icon_2I_fcast_cineca_get.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# source common get_ procedures
+. `dirname $0`/cineca_icon_get_common.sh
+
+get_init() {
+    export PROCNAME=fcast_cineca_get
+    export EXTRA_CONF=icon_2I/
+    export ECF_MONITOR=
+}
+
+icon_postproc() {
+    IFS=_ read -ra keys <<< $1
+    case ${keys[4]} in # verify [4]
+    *Pv*)
+        putarki_configured_archive $2 $1 grib
+        lami_make_crossg2 $1 ${1}_crossg2
+        [ -s "${1}_crossg2" ] && putarki_configured_archive $2 ${1}_crossg2 $POSTPROC_FORMAT
+        rm -f ${1}_crossg2
+        lami_make_vprofg2 $1 ${1}_vprofg2
+        [ -s "${1}_vprofg2" ] && putarki_configured_archive $2 ${1}_vprofg2 $POSTPROC_FORMAT
+        rm -f ${1}_vprofg2
+        ;;
+
+     *A0* | *A1* | *A2* | *A3*)
+        putarki_configured_archive $2 $1 grib
+        ;;
+    esac
+}
+
+
+set -x
+export POSTPROC_FUNC=icon_postproc
+# enter main loop
+main_loop "$@"
