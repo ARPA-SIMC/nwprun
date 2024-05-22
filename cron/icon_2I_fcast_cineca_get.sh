dca1bad8ba5cf40e3618d157723cba2214bed5df
Davide Cesari <dcesari@arpae.it>
2024-05-22 17:43:41 +0200

    use a local directory for output files due to original not being writable

--- cron/icon_2I_fcast_cineca_get.sh
+++ cron/icon_2I_fcast_cineca_get.sh
@@ -14,12 +14,13 @@ icon_postproc() {
     case ${keys[4]} in # verify [4]
     *Pv*)
         putarki_configured_archive $2 $1 grib
-        lami_make_crossg2 $1 ${1}_crossg2
-        [ -s "${1}_crossg2" ] && putarki_configured_archive $2 ${1}_crossg2 $POSTPROC_FORMAT
-        rm -f ${1}_crossg2
-        lami_make_vprofg2 $1 ${1}_vprofg2
-        [ -s "${1}_vprofg2" ] && putarki_configured_archive $2 ${1}_vprofg2 $POSTPROC_FORMAT
-        rm -f ${1}_vprofg2
+        tmpbase=$LAMI_CINECA_WORKDIR/${1}
+        lami_make_crossg2 $1 ${tmpbase}_crossg2
+        [ -s "${tmpbase}_crossg2" ] && putarki_configured_archive $2 ${tmpbase}_crossg2 $POSTPROC_FORMAT
+        rm -f ${tmpbase}_crossg2
+        lami_make_vprofg2 $1 ${tmpbase}_vprofg2
+        [ -s "${tmpbase}_vprofg2" ] && putarki_configured_archive $2 ${tmpbase}_vprofg2 $POSTPROC_FORMAT
+        rm -f ${tmpbase}_vprofg2
         ;;
 
      *A0* | *A1* | *A2* | *A3*)
