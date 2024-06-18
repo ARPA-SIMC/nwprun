8655cdd30c033d3175b7a5e855d39a4ce8c26f81
Davide Cesari <dcesari@arpae.it>
2024-06-18 11:28:00 +0200

    modification by Thomas: add test suite for ICON-2I-KENDA and reorganize initialization namelist

--- ecflow/icon_test_suite.py
+++ ecflow/icon_test_suite.py
@@ -62,3 +62,44 @@ for h in range(0, 24, 12):
 icont.check()
 icont.write(interactive=interactive)
 icont.replace(interactive=interactive)
+
+
+# Suite enda test
+extra_env = common_extra_env.copy()
+extra_env.update({
+    "NWPCONF": "test/icon_2I/enda",
+    "NNODES_PREMODEL": 1,
+    "NNODES_MODEL": 3,
+    "NNODES_ENDA": 6
+})
+basicenv = BasicEnv(srctree=os.path.join(os.environ["WORKDIR_BASE"], "nwprun"),
+                    worktree=os.path.join(os.environ["WORKDIR_BASE"], "ecflow"),
+                    sched="slurm",
+                    client_wrap=os.path.join(os.environ["WORKDIR_BASE"], "nwprun","ecflow","ec_wrap"),
+                    ntries=2,
+                    extra_env=extra_env)
+
+conf = ModelConfig({"gts": True, "lhn": True, "radarvol": True, "membrange": "0-40",
+                    "postprocrange": "-1",
+                    "modelname": "icon",
+                    "runlist": [GetObs, EpsMembers, EndaAnalysis],
+                    "preproc_wt":"00:20:00", "model_wt": "01:00:00"}).getconf()
+enda = ModelSuite("icon_2I_enda_test")
+basicenv.add_to(enda.suite)
+day = enda.suite.add_family("day").add_repeat(
+    ecflow.RepeatDate("YMD",
+                      int((datetime.datetime.now()-datetime.timedelta(days=delta[0])).strftime("%Y%m%d")),
+                      20301228))
+
+hdep = None # first repetition has no dependency
+for h in range(0, 24, 1):
+    famname = "hour_" + ("%02d" % h)
+    hour = day.add_family(famname).add_variable("TIME", "%02d" % h)
+    #    hrun = "%02d:00" % (h+1 % 24) # start 1h after nominal time
+    WaitAndRun(dep=hdep, conf=conf).add_to(hour)
+    hdep = famname # dependency for next repetition
+
+enda.check()
+enda.write(interactive=interactive)
+enda.replace(interactive=interactive)
+
