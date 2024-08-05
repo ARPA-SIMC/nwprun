a0dcaf8303b50302e88ebedd1f13cc78fa0d17dd
Davide Cesari <dcesari@arpae.it>
2024-08-05 14:41:27 +0200

    mass update, reformat the signal trap system in jobs with a new head.h, it is necessary to add a tail.h at the end of every job

--- ecflow/include_slurm/head.h
+++ ecflow/include_slurm/head.h
@@ -35,23 +35,24 @@ fi
 # -1 control
 # -2 loop on all members from 0 to $ENS_TOTAL_MEMB
 
-# Tell ecFlow we have started
-$ecflow_client --init=$ECF_RID
-
 # Define error and exit handlers
 ERROR() {
     set +e                      # Clear -e flag, so we don't fail
-    wait                        # wait for background process to stop
+    errmsg="$2"
+    if [ "$1" = "0" ]; then
+        errmsg="CANCELLED or TIMED OUT"
+    fi
     if [ "%NO_FAIL:%" = "TRUE" ]; then
     $ecflow_client --msg="Forgiving failure of %ECF_NAME%"
     $ecflow_client --complete   # Notify ecFlow of a normal end
     else
-    $ecflow_client --abort=trap # Notify ecFlow that something went wrong, using 'trap' as the reason
+    $ecflow_client --abort="$errmsg" # Notify ecFlow that something went wrong
     fi
-    trap - EXIT ERR             # Remove the trap
+    trap - 0 ERR $SIGNAL_LIST            # Remove the trap
     exit 0                      # End the script, was exit 1, set to 0 to avoid double failure of interactive jobs
 }
  
+# useless, remove
 CLEANEXIT() {
     [ "$?" = "0" ] || ERROR   # required for `exit 1` to call ERROR function
     wait                      # wait for background process to stop
@@ -60,13 +61,37 @@ CLEANEXIT() {
     exit 0                    # End the script
 }
  
+# this function has to be explicitly called for exiting the job
+# without error, either at the end (done in tail.h) or anywhere in the
+# middle if necessary, otherwise a plain exit 0 will be treated as an
+# scancel/timeout situation (for a slurm job); exit 1 is allowed for
+# forcing an exit with error
+exit_0() {
+    wait                     # wait for background process to stop
+    $ecflow_client --complete # Notify ecFlow of a normal end
+    trap - 0                 # Remove all traps
+    exit 0                   # End the script
+}
+
+# Trap any signal that may cause the script to fail; note: don't trap
+# SIGTERM/SIGCONT for Slurm to properly reach shell children on
+# cancel/timeout
+export SIGNAL_LIST='1 2 3 4 5 6 7 8 10 11 13 24 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64'
+
+for signal in $SIGNAL_LIST; do
+  trap "ERROR $signal \"Signal $(kill -l $signal) ($signal) received \"" $signal
+done
+
 # Trap any calls to exit and errors caught by the -e flag
-#trap ERROR 0
+trap "ERROR \$? \"EXIT code \$?\"" 0
  
 # Trap any signal that may cause the script to fail
-trap '{ CLEANEXIT ; }' EXIT
-trap '{ echo "Exiting with error"; ERROR ; }' ERR
-trap '{ echo "Killed by a signal"; ERROR ; }' 1 2 3 4 5 6 7 8 10 11 13 24
+#trap '{ CLEANEXIT ; }' EXIT
+#trap '{ echo "Exiting with error"; ERROR ; }' ERR
+#trap '{ echo "Killed by a signal"; ERROR ; }' 1 2 3 4 5 6 7 8 10 11 13 24
+
+# Tell ecFlow we have started
+$ecflow_client --init=$ECF_RID
 
 # diagnostics
 env|grep SLURM || true
