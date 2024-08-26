a6fdc8d52f75d588f1f6355403e1c3c8462f3ff2
Davide Cesari <dcesari@arpae.it>
2024-08-26 15:12:52 +0200

    modify container creation for some problematic Airep reports

--- sylabs_deploy.sh
+++ sylabs_deploy.sh
@@ -2,7 +2,7 @@
 
 singularity remote list
 echo "In case of error you may need to get a new access token"
-echo "from cloud.sylabs.io and execute singularity remote login"
+echo "from cloud.sylabs.io by executing `singularity remote login`"
 
 set -e
 set -x
