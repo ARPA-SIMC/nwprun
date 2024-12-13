d924356914b6b732833ca47e7eea116ce1865930
Davide Cesari <dcesari@arpae.it>
2024-12-13 14:27:21 +0000

    implement icon-2I nested in icon-eu suite (working up to iconremap excluded)

---
+++ conf/prod/icon_2I/fci2i/conf.sh
@@ -0,0 +1,71 @@
+# Parent model environment variables
+PARENTMODEL=ICON
+PARENTMODEL_ARKI_DS=$ARKI_DIR/icon_eu_dwd_foricon
+PARENTMODEL_SIGNAL=iconeu_dwd_foricon
+PARENTMODEL_FREQINI=12
+PARENTMODEL_FREQANA=12
+PARENTMODEL_FREQFC=1
+PARENTMODEL_STATIC=$HOME/data/icon
+PARENTMODEL_STATICFILE=iconeu_dwd_foricon_extpar_R03B08_N02_20231113_tiles.nc
+
+# Model environment variables
+MODEL_BASE=$HOME/srcgnu/icon
+MODEL_BIN=$MODEL_BASE/bin/icon
+ECRAD_DATA=$MODEL_BASE/data
+MODEL_STATIC=$HOME/data/icon
+MODEL_PRE_BINDIR=$HOME/srcgnu/dwd_icon_tools/icontools
+
+# Dataset arkimet for observations
+ARKI_LHN_DS=$ARKI_DIR/icon_2I_radar
+MODEL_LHN_DT=600
+
+# ICON-2I domain and grid files
+DOMAIN=ICON_REG3_DOM01	
+LOCALGRID=$MODEL_STATIC/domain_$DOMAIN/${DOMAIN}.nc
+LOCALGRID_PARENT=$MODEL_STATIC/domain_$DOMAIN/${DOMAIN}.parent.nc
+LOCALGRID_EXTERNAL=$MODEL_STATIC/domain_$DOMAIN/external_parameter_icon_${DOMAIN}_tiles.nc
+
+# Model environment variables
+MODEL_BACK=0
+MODEL_STOP=48 #72
+MODEL_BCANA=N
+MODEL_FREQINI=12
+ENS_TOTAL_MEMB=0
+
+# Time difference between model and parent reftime 
+MODEL_DELTABD=0
+
+# Number of boundary conditions handled by each task
+NBC_PER_TASK=1
+
+# Latent Heat Nudging (LHN)
+MODEL_LHN=.TRUE.
+MODEL_NH_LHN=4
+
+# eccodes definitions
+ecd_dwd=$WORKDIR_BASE/data/definitions/definitions.edzw-2.21.0-1
+ecd_cin=$WORKDIR_BASE/data/definitions/definitions.cineca-2.21.0
+ECCODES_DEFINITION_PATH=${ecd_dwd}:${ecd_cin}
+unset ecd_dwd ecd_cin
+
+# setup for arkilocal
+ARKI_DIR=$WORKDIR/arki
+# setup for remote import
+unset ARKI_IMPDIR
+#ARKI_SYNCDIR=$WORKDIR_BASE/import/sync.ftpmodelin
+unset ARKI_DLDIR
+CROSS_NETWORK=icon_2I_fci2i_c
+VPROF_NETWORK=icon_2I_fci2i_v
+MODEL_SIGNAL=icon_2I_fci2i
+
+# suite timing
+NWPWAITELAPS=14400
+# differenza tra tempo nominale e tempo di attivazione della suite
+NWPWAITSOLAR_RUN=5400
+# dopo quando tempo rinuncio a girare la suite e passare alla successiva
+NWPWAITSOLAR=36000
+NWPWAITWAIT=60
+# wait for analysis?
+WAIT_ANALYSIS=N
+# to be removed
+STOP_ON_FAIL=Y
---
+++ ecflow/arpae_icon_suites.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+import os,sys
+import datetime
+import optparse
+import ecflow
+from nwprun import *
+
+parser = optparse.OptionParser(usage="%prog [OPTIONS]")
+parser.add_option("--yes", help="work in non-interactive mode and answer yes to all questions (it will overwrite files and replace scripts on server)",
+                  action="store_true")
+parser.add_option("--delta", help="delta time in days to go back", default=0)
+
+opts, args = parser.parse_args()
+interactive = not opts.yes
+delta = opts.delta
+
+common_extra_env = {
+    "NO_FAIL": "FALSE",
+    "TASK_PER_CORE": "1",
+    "HPCENV": os.environ["HPC_SYSTEM"],
+    "ECF_TIMEOUT": "7200",
+    "ECF_DENIED": ""
+}
+
+# Suite icon fci2i
+extra_env = common_extra_env.copy()
+extra_env.update({
+    "NWPCONF": "prod/icon_2I/fci2i",
+    "NNODES_PREMODEL": 2,
+    "NNODES_MODEL": 8,
+    "NTASKS_PREMODEL": 64,
+    "NTASKS_MODEL": 256
+})
+basicenv = BasicEnv(srctree=os.path.join(os.environ["WORKDIR_BASE"], "nwprun"),
+                    worktree=os.path.join(os.environ["WORKDIR_BASE"], "ecflow"),
+                    sched="slurm",
+                    client_wrap="",
+                    ntries=2,
+                    extra_env=extra_env)
+
+conf = ModelConfig({"gts": False, "lhn": True, "membrange": "0",
+                    "postprocrange": "0",
+                    "modelname": "icon", 
+                    "runlist": [GetObs, EpsMembers],
+                    "preproc_wt":"00:20:00", "model_wt": "03:00:00"}).getconf()
+icon = ModelSuite("icon_2I_fci2i")
+basicenv.add_to(icon.suite)
+day = icon.suite.add_family("day").add_repeat(
+    ecflow.RepeatDate("YMD", 
+                      int((datetime.datetime.now()-datetime.timedelta(days=delta)).strftime("%Y%m%d")),
+                      20301228))
+
+hdep = None # first repetition has no dependency
+for h in range(0, 24, 12):
+    famname = "hour_" + ("%02d" % h)
+    hour = day.add_family(famname).add_variable("TIME", "%02d" % h)
+    #    hrun = "%02d:00" % (h+1 % 24) # start 1h after nominal time
+    WaitAndRun(dep=hdep, conf=conf).add_to(hour)
+    hdep = famname # dependency for next repetition
+
+icon.check()
+icon.write(interactive=interactive)
+icon.replace(interactive=interactive)
+
+
