e65b08ec70ea9bb4142be089551fe46430c5f121
Davide Cesari <dcesari@arpae.it>
2025-12-02 10:36:37 +0100

    Define exit_0 function if not defined, partially addressing #28

--- ecflow/include_slurm/nwpconf.h
+++ ecflow/include_slurm/nwpconf.h
@@ -1,7 +1,14 @@
+# source desired  NWPCONF configuration
 if [ -n "$NWPCONF" ]; then
     export NWPCONFDIR=$WORKDIR_BASE/nwprun/conf
     export NWPCONFBINDIR=$WORKDIR_BASE/nwprun/libexec/nwpconf
 # source the main nwpconf library module, other modules must be sourced
 # in the job
     . $NWPCONFBINDIR/nwpconf.sh
+fi
+# define exit_0 function if not defined
+if [ ! $(type -t exit_0) ]; then
+    exit_0() {
+	exit 0
+    }
 fi
